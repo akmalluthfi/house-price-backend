1461ea8a4d8971d1043ced7ad531293d418e7232
Akmal Luthfi <akmalluthfi19@gmail.com>
2025-05-11 17:07:25 +0700

    add config & env

--- app/main.py
+++ app/main.py
@@ -1,14 +1,38 @@
-from typing import Union
+import os
 from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+from dotenv import dotenv_values
+from app.core.config import settings
+from fastapi.routing import APIRoute
+from starlette.middleware.cors import CORSMiddleware
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def custom_generate_unique_id(route: APIRoute) -> str:
+    return f"{route.tags[0]}-{route.name}"
+
+
+if settings.ENVIRONMENT == "local":
+    os.environ.update(dotenv_values(".env"))
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME, generate_unique_id_function=custom_generate_unique_id
+)
+
+# Set all CORS enabled origins
+if settings.all_cors_origins:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.all_cors_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+
+@app.get("/", tags=["root"], response_model=dict)
+def read_root() -> dict:
+    return {
+        "project": settings.PROJECT_NAME,
+        "version": "1.0",
+        "info": "Check the /docs routes",
+    }
